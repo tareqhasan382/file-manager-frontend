a2dd4ae60eb34440bb5349d00bffab7d95f5b865
unknown <tareqhasan382@gmail.com>
2026-02-25 16:43:54 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className=' w-full flex items-center justify-center text-purple-500 '><h1 className=' font-bold text-3xl text-center '>SaaS File Management System</h1></div>
+    </>
+  )
+}
+
+export default App
