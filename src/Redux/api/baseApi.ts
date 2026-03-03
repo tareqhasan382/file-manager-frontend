298647366950c9f65676d1404cf081b0385a77de
unknown <tareqhasan382@gmail.com>
2026-03-03 22:36:03 +0600

    change base url

--- src/Redux/api/baseApi.ts
+++ src/Redux/api/baseApi.ts
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-export const BASEURL = "http://localhost:8000";
+import { BASE_URL } from "../../App";
+export const BASEURL = BASE_URL;
 
 export const baseApi = createApi({
   reducerPath: "api",
